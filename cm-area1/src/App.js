1e2d3caca53d3b0c3d5e3d184075c749b69154a7
artjitt9611 <artjitt2512@gmail.com>
2022-09-21 12:48:39 +0700

    Deploy success

--- cm-area1/src/App.js
+++ cm-area1/src/App.js
@@ -1,10 +1,24 @@
-
+import { Route, HashRouter ,Routes,Link} from "react-router-dom";
+import Home from "./Page/Home"
+import About from "./Page/About"
+import Profile from "./Page/Profile"
 function App() {
-  return (
-    <div className="App">
-    <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+            <HashRouter>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/profile">Profile</Link>
+            </nav>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+
+            </Routes>
+              
+            </HashRouter>
+    );
 }
 
 export default App;
